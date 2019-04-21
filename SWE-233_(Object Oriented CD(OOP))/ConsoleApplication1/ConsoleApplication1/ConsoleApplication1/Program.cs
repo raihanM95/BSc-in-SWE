f259df2e2c392dddaa8adc161245bc600f40fd09
raihanM95 <mahmudrony95@gmail.com>
2019-04-21 23:01:21 +0600

    10 sem

---
+++ SWE-233_(Object Oriented CD(OOP))/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Customer
+    {
+        private int _phone;
+        private int _pin;
+        private string _name;
+        private int _id;
+        private double _balance;
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public double balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+        public void register(int phone, int pin, string name, int id, double balance)
+        {
+            _phone = phone;
+            _pin = pin;
+            _name = name;
+            _id = id;
+            _balance= balance;
+            Console.WriteLine("Before Update Phone: " +_phone+ " Pin: " +_pin+ " Name:" + _name+ " Id:"+_id+ " Balance:"+_balance);
+        }
+        public void update(int phone, int pin, string name, double balance)
+        {
+            _phone = phone;
+            _pin = pin;
+            _name = name;
+            _balance= balance;
+            Console.WriteLine("After Update Phone: " + _phone + " Pin: " + _pin + " Name:" + _name + " Balance:" + _balance);
+        }
+
+        public void withdraw(int i)
+        {
+            _balance = _balance - i;
+        }
+        public void deposit(int i)
+        {
+            _balance = _balance + i;
+        }
+    }
+    class Agent
+    {
+        private int _id;
+        private string _name;
+        public void register(int id, string name)
+        {
+            _id=id;
+            _name= name;
+        }
+        public void registercustome(Customer c)
+        {
+            c.register(01716, 012, "Somebody", 1, 500);
+
+        }
+        public void updatecustomer(Customer c)
+        {
+            c.update(01716, 012, "Ihsan ahmed", 1000);
+
+        }
+        public void print(Customer cus)
+        {
+            Console.WriteLine("Customer name: " +cus.name+ "   Current balance: " + cus.balance);
+        }
+
+    }
+    class Manager
+    {
+        private int _id;
+        private string _name;
+        public Manager(int id, string name)
+        {
+            _id=id;
+            _name= name;
+        }
+        public void registeragent(Agent ag)
+        {
+            ag.register(1,"Habib");
+        }
+
+    }
+    class Mobilebanking
+    {
+        static void Main(string[] args)
+        {
+            Agent ag = new Agent();
+            Manager man= new Manager(1, "Moon");
+            man.registeragent(ag);
+            Customer cus = new Customer();
+            ag.registercustome(cus);
+            ag.updatecustomer(cus);
+            cus.withdraw(100);
+            cus.deposit(500);
+            ag.print(cus);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ SWE-233_(Object Oriented CD(OOP))/Last_Mid_Lab_Solution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class order
+    {
+        string id;
+        double price;
+        public void calcTax()
+        {
+            price += price * 5 / 100;
+        }
+        public void take_price(double p)
+        {
+            price = p;
+        }
+        class audience
+        {
+            string id;
+            string name;
+        }
+        class user : audience
+        {
+            public void placeOrdera(order o)
+            {
+                Console.WriteLine("Press c to continue more order");
+                string input = Console.ReadLine();
+                if (input == "c")
+                {
+                    o.take_price(10000);
+                }
+                else
+                    Console.WriteLine("Invalid Input");
+            }
+        }
+        class manager : audience
+        {
+            public void calcDioscount(order o)
+            {
+                if (o.price > 5000)
+                {
+                    o.price -= o.price * 7 / 100;
+                    Console.WriteLine("Total Price is " + o.price);
+                }
+                else
+                    Console.WriteLine("Total price is " + o.price);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                order o = new order();
+                o.take_price(7000);
+                o.calcTax();
+                manager m = new manager();
+                m.calcDioscount(o);
+                user u = new user();
+                u.placeOrdera(o);
+                o.calcTax();
+                m.calcDioscount(o);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
